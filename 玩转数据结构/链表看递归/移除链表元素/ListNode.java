5700a5b36d24b86d595ccc597cb0bcac1aa90883
bennyrhys <bennyrhys@163.com>
2020-02-27 22:22:43 +0800

    链表看递归-两种处理头节点_移除链表元素

---
+++ 玩转数据结构/链表看递归/移除链表元素/ListNode.java
@@ -0,0 +1,12 @@
+package 玩转数据结构.链表看递归.移除链表元素;
+
+/**
+ * @Author bennyrhys
+ * @Date 2020-02-27 21:45
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ 玩转数据结构/链表看递归/移除链表元素/Solution2.java
@@ -0,0 +1,55 @@
+package 玩转数据结构.链表看递归.移除链表元素;
+
+/**
+ * 法2：虚拟头节点
+ * @Author bennyrhys
+ * @Date 2020-02-27 21:43
+ */
+class Solution2 {
+    public ListNode removeElements(ListNode head, int val) {
+        /**
+         * 思考：通过删除之前的节点删除指定节点。
+         *
+         * 头节点没有之前的节点
+         * 两种处理：
+         * 1、特殊处理头
+         * 2、虚拟头节点
+         */
+
+        // 1、特殊处理头,删除开始部分的全部节点
+        while (head != null && head.val == val) {
+//            优化
+//            ListNode delNode = head;
+//            head = head.next;
+//            delNode.next = null;
+
+//            简化
+            head = head.next;
+        }
+
+        // 所有的节点都为空
+        if (head == null) {
+            return null;
+        }
+
+        // 删除链表中间，设置通过前一个节点
+        ListNode prev = head; // 此时head肯定不为删除节点，后面的可以是删除节点
+
+        // 如果没有到结尾
+        while (prev.next != null) {
+            // 如果后一个节点匹配
+            if (prev.next.val == val) {
+//                优化
+//                ListNode delNode = prev.next;
+//                prev.next = delNode.next;
+//                delNode.next = null;
+
+//                简化
+                prev.next = prev.next.next;
+            } else
+                prev = prev.next;
+        }
+
+        return head;
+    }
+}
