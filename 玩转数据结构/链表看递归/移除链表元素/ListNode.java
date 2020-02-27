61781467008f145c4b621d661de0af56c457b94f
bennyrhys <bennyrhys@163.com>
2020-02-27 22:52:37 +0800

    链表看递归-提供可以本地调试的环境

--- 玩转数据结构/链表看递归/移除链表元素/ListNode.java
+++ 玩转数据结构/链表看递归/移除链表元素/ListNode.java
@@ -9,4 +9,38 @@ public class ListNode {
     int val;
     ListNode next;
     ListNode(int x) { val = x; }
+
+    // 构造函数-转数组
+    // 转化数组变成链表，通过ListNode实现
+    public ListNode(int[] arr){
+
+        // 判空-空数组
+        if (arr == null && arr.length == 0)
+            throw new IllegalArgumentException("arr can not be empty");
+
+        // 头节点赋值
+        this.val = arr[0];
+        // 当前链表头
+        ListNode cur = this;
+
+        // 循环赋值
+        for (int i : arr) {
+            cur.next = new ListNode(i);
+            cur = cur.next;
+        }
+    }
+
+    // 便于打印
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        ListNode cur = this;
+        res.append("ListNode:");
+        while (cur != null) {
+            res.append(cur.val + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
 }
--- 玩转数据结构/链表看递归/移除链表元素/Main.java
+++ 玩转数据结构/链表看递归/移除链表元素/Main.java
@@ -7,9 +7,34 @@ package 玩转数据结构.链表看递归.移除链表元素;
  *
  * 输入: 1->2->6->3->4->5->6, val = 6
  * 输出: 1->2->3->4->5
- * 
+ *
  * @Author bennyrhys
  * @Date 2020-02-27 21:43
  */
 public class Main {
+    // 构建本地测试用例，提供可以本地调试的环境，创建ListNode中构造函数构建链表
+
+    /**
+     * ListNode:1->1->2->2->3->6->4->6->NULL
+     * res1:ListNode:1->1->2->2->3->4->NULL
+     * res2:ListNode:1->1->2->2->3->4->NULL
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // 转化数组变成链表，通过ListNode实现
+        int[] arr = {1, 2, 2, 3, 6, 4, 6};
+
+        // 转化成链表并检验
+        ListNode head = new ListNode(arr);
+        System.out.println(head);
+
+        // 测试1 特殊处理头的-移除元素
+        ListNode res1 = (new Solution()).removeElements(head, 6);
+
+        // 测试2 虚拟头节点-移除元素
+        ListNode res2 = (new Solution2()).removeElements(head, 6);
+
+        System.out.println("res1:"+res1+"\n"+"res2:"+res2);
+    }
 }
