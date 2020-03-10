eb4551eecd90c24676206a0373d08040a3fe9fd6
bennyrhys <bennyrhys@163.com>
2020-03-10 09:52:20 +0800

    当前线程

---
+++ 剑指Java/Java多线程与并发/CurrentThreadDemo.java
@@ -0,0 +1,16 @@
+package 剑指Java.Java多线程与并发;
+
+/**
+ * 当前线程
+ * @Author bennyrhys
+ * @Date 2020-03-10 09:51
+ */
+public class CurrentThreadDemo {
+    /**
+     * main
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
