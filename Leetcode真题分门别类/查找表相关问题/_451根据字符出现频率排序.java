72de8ac73638275f9a0fbe7d78c8f636b4900186
bennyrhys <bennyrhys@163.com>
2020-03-23 20:34:03 +0800

    _451根据字符出现频率排序-数组计数-map组串

---
+++ Leetcode真题分门别类/查找表相关问题/_451根据字符出现频率排序.java
@@ -0,0 +1,63 @@
+package Leetcode真题分门别类.查找表相关问题;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @Author bennyrhys
+ * @Date 2020-03-23 18:20
+ * 注意：
+ * 大小写敏感
+ *
+ * 思路：
+ * map记频
+ */
+public class _451根据字符出现频率排序 {
+
+    public String frequencySort(String s) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        int[] freq = new int[256];
+        for (char c : s.toCharArray()) {
+            freq[c]++;
+        }
+
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                char ch = (char) i;
+
+                String str = map.get(freq[i]);
+                // 字符出现次数相同, 进行拼接
+                if (str != null) {
+                    String strNew = str.concat(build(ch, freq[i]));
+                    map.put(freq[i], strNew);
+                }else {
+                    map.put(freq[i], build(ch, freq[i]));
+                }
+            }
+        }
+
+        Integer[] keys = map.keySet().toArray(new Integer[]{});
+        Arrays.sort(keys);
+        StringBuilder sbl = new StringBuilder();
+        for (int i = keys.length - 1; i >= 0; i--) {
+            sbl.append(map.get(keys[i]));
+        }
+
+        return sbl.toString();
+    }
+
+    private String build(char ch, int times) {
+        String string = Character.toString(ch);
+        StringBuilder res = new StringBuilder(string);
+        int t = 1;
+        while (t < times) {
+            res.append(string);
+            t++;
+        }
+
+        return res.toString();
+    }
+}
