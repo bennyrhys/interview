a7a2e097cb8983b21d8c27a6ad8a79823bad12bc
bennyrhys <bennyrhys@163.com>
2020-05-24 17:12:19 +0800

    _217存在重复元素

--- Leetcode真题分门别类/查找表相关问题/_217存在重复元素.java
+++ Leetcode真题分门别类/查找表相关问题/_217存在重复元素.java
@@ -1,11 +1,24 @@
 package Leetcode真题分门别类.查找表相关问题;
 
+import java.util.HashSet;
+
 /**
  * @Author bennyrhys
  * @Date 2020-05-24 17:05
+ * 思路：
+ * set不重复元素形成的查找表
+ *
+ * 复杂度：
+ * 时间O（n）
+ * 空间O（n）
  */
 public class _217存在重复元素 {
     public boolean containsDuplicate(int[] nums) {
-
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+        return false;
     }
 }
